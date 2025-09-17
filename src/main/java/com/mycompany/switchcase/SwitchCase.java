5cd1a52662100bee9fffea563352a1171e9d5947
Ana Fagundes <anabeatrizafagundes@gmail.com>
2025-09-17 14:42:57 -0300

    switch case commit

--- src/main/java/com/mycompany/switchcase/SwitchCase.java
+++ src/main/java/com/mycompany/switchcase/SwitchCase.java
@@ -1,27 +1,27 @@
 package com.mycompany.switchcase;
 
-import java.util.Scanner;
+import java.util.Scanner;// importei a classe scanner
 
-public class SwitchCase {
+public class SwitchCase {// denominei de SwitchCase 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Digite o seu número de erros na prova de 1 a 10: ");
+        try (Scanner scanner = new Scanner(System.in)) {// o try serve para evitar vazamento de dados, e também essa parte fica responsável pela leitura da entrada de dados do teclado do usuário
+            System.out.print("Digite o número de cômodos de sua casa ");// aqui é perguntado ao usuário a quantidade de cômodos da casa do usuário
             int resposta = scanner.nextInt();
 
             switch (resposta) {
                 case 1:
-                    System.out.println("Você foi excelente, continue assim!");
+                    System.out.println("Como? Impossível🤯");
                     break;
                 case 4:
                 case 5:
-                case 6:
-                    System.out.println("Você foi bem, busque sempre melhorar!");
+                    System.out.println("É pequeno, mas atende as suas necessidades?");
                     break;
+                case 6:
                 case 7:
                 case 8:
                 case 9:
                 case 10:
-                    System.out.println("Você precisa urgentemente contatar seu professor e fazer uma prova de recuperação");
+                    System.out.println("Sua casa é de médio a grande porte, pelo visto ela é bem espaçosa 👌😎");
                     break;
                 default:
                     System.out.println("Valor inválido! Digite de 1 a 10.");
